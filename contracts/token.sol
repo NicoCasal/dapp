ef469553c90a3b5610d788b0ed8a58c77485891a
NicoCasal <martin.casal.venencia@gmail.com>
2024-09-18 16:53:13 -0300

    add contracts

--- contracts/token.sol
+++ contracts/token.sol
@@ -1,5 +1,12 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
-import "hardhat/console.sol";
 
-contract token {}
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract token is ERC20 {
+    constructor() ERC20("ReMaTh", "RTW") {
+        _mint(msg.sender, 20);
+    }
+}
